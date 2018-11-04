2b770d36db6c80c15e86567ae77e2c6f91904c94
hjc981024 <450342251@qq.com>
2018-11-04 22:06:25 +0800

    fix conflict

--- Sources/Framework/framework_base.h
+++ Sources/Framework/framework_base.h
@@ -20,7 +20,7 @@ public:
     framework_block_item* current_status();//返回游戏方块数组指针
     number_block* generate_block(); // 生成数字块
     void merge(); // 合并
-
+    void printGameBoard(); // 输出游戏局面
 	//Han
     void setMovingBlock(number_block * pNumBlock);  //  修改类内成员变量moving_block
 
